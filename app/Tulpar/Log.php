cbbfa05de146b61a524cff1f3402847584561ec0
Isa Eken <hello@isaeken.com.tr>
2021-08-10 13:54:22 +0300

    check administrator permission on logging

--- app/Tulpar/Log.php
+++ app/Tulpar/Log.php
@@ -9,53 +9,69 @@ use Illuminate\Support\Facades\Log as FacadesLog;
 
 class Log
 {
-    public static function emergency($message, array $context = array())
+    public static function emergency($message, array $context = [])
     {
         static::log('emergency', $message);
     }
 
-    public static function alert($message, array $context = array())
+    public static function alert($message, array $context = [])
     {
         static::log('alert', $message);
     }
 
-    public static function critical($message, array $context = array())
+    public static function critical($message, array $context = [])
     {
         static::log('critical', $message);
     }
 
-    public static function error($message, array $context = array())
+    public static function error($message, array $context = [])
     {
         static::log('error', $message);
     }
 
-    public static function warning($message, array $context = array())
+    public static function warning($message, array $context = [])
     {
         static::log('warning', $message);
     }
 
-    public static function notice($message, array $context = array())
+    public static function notice($message, array $context = [])
     {
         static::log('notice', $message);
     }
 
-    public static function info($message, array $context = array())
+    public static function info($message, array $context = [])
     {
         static::log('info', $message);
     }
 
-    public static function debug($message, array $context = array())
+    public static function debug($message, array $context = [])
     {
         static::log('debug', $message);
     }
 
-    public static function log($level, $message, array $context = array())
+    public static function log($level, $message, array $context = [])
     {
         FacadesLog::log($level, $message, $context);
         try {
-            Tulpar::getInstance()->getLogChannel()?->sendMessage(
-                '``' . strtoupper($level) . '``: ' . $message
-            );
+            $channel = Tulpar::getInstance()->getLogChannel();
+            if ($channel === null) {
+                echo PHP_EOL . PHP_EOL . '!!! WARN: Log channel is not selected !!!' . PHP_EOL . PHP_EOL;
+            }
+            else {
+                if (!Tulpar::getInstance()->checkPermission($channel->guild, 'administrator', true)) {
+                    echo PHP_EOL . PHP_EOL . '!!! ERR: Missing "administrator" permission in application server !!!' . PHP_EOL . PHP_EOL;
+                    Tulpar::getInstance()->getDiscord()->getLoop()->stop();
+                    exit;
+                }
+
+                if (in_array($level, ['emergency', 'alert', 'critical', 'error', 'warning', 'notice', 'info', 'debug'])) {
+                    if (config('tulpar.server.logging.' . $level) == true) {
+                        $channel->sendMessage(
+                            '``' . strtoupper($level) . '``: ' . $message
+                        );
+                    }
+                }
+            }
         } catch (Exception $exception) {
             // ...
         }
