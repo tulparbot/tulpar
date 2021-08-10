7d156a3783868747e941f335119cdd8ed61c8314
Isa Eken <hello@isaeken.com.tr>
2021-08-10 18:10:30 +0300

    updated invite command

--- app/Tulpar/Commands/Basic/InviteCommand.php
+++ app/Tulpar/Commands/Basic/InviteCommand.php
@@ -6,6 +6,7 @@ namespace App\Tulpar\Commands\Basic;
 
 use App\Tulpar\Commands\BaseCommand;
 use App\Tulpar\Contracts\CommandInterface;
+use Illuminate\Support\Str;
 
 class InviteCommand extends BaseCommand implements CommandInterface
 {
@@ -17,8 +18,21 @@ class InviteCommand extends BaseCommand implements CommandInterface
 
     public static bool $allowPm = true;
 
+    public static array $usages = [''];
+
+    public static string $version = '1.1';
+
     public function run(): void
     {
-        $this->message->channel->sendMessage('There is my invite link: ' . $this->discord->application->invite_url);
+        $url = 'https://discord.com/api/oauth2/authorize?client_id={client_id}&permissions={permissions}&scope={scope}';
+        $client_id = config('discord.client.id');
+        $permissions = 8;
+        $scopes = ['bot'];
+        $url = Str::of($url)
+            ->replace('{client_id}', urlencode($client_id))
+            ->replace('{permissions}', urlencode($permissions))
+            ->replace('{scope}', implode('%20', $scopes));
+
+        $this->message->reply('🥰 There is my invite link: ' . $url);
     }
 }
