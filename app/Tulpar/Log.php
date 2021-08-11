66b9c428d8590a59fc2488c316335fb742688e61
Isa Eken <hello@isaeken.com.tr>
2021-08-11 18:12:52 +0300

    updated logger

--- app/Tulpar/Log.php
+++ app/Tulpar/Log.php
@@ -4,76 +4,112 @@
 namespace App\Tulpar;
 
 
+use Discord\Parts\Channel\Channel;
+use Discord\Parts\Channel\Message;
 use Exception;
 use Illuminate\Support\Facades\Log as FacadesLog;
 
 class Log
 {
+    private static Channel|null|false $channel = null;
+
+    private static function getChannel(): Channel|null
+    {
+        if (static::$channel === false) {
+            return null;
+        }
+
+        if (static::$channel === null) {
+            static::$channel = Tulpar::getInstance()->getLogChannel();
+            if (static::$channel === null) {
+                static::consoleLog('Log channel is not selected');
+                static::$channel = false;
+                return null;
+            }
+        }
+
+        return static::$channel;
+    }
+
+    private static function consoleLog(string $text, string $level = 'WARN'): void
+    {
+        echo PHP_EOL . PHP_EOL . '!!! ' . $level . ': ' . $text . ' !!!' . PHP_EOL . PHP_EOL;
+    }
+
     public static function emergency($message, array $context = [])
     {
-        static::log('emergency', $message);
+        static::log('emergency', $message, $context);
     }
 
     public static function alert($message, array $context = [])
     {
-        static::log('alert', $message);
+        static::log('alert', $message, $context);
     }
 
     public static function critical($message, array $context = [])
     {
-        static::log('critical', $message);
+        static::log('critical', $message, $context);
     }
 
     public static function error($message, array $context = [])
     {
-        static::log('error', $message);
+        static::log('error', $message, $context);
     }
 
     public static function warning($message, array $context = [])
     {
-        static::log('warning', $message);
+        static::log('warning', $message, $context);
     }
 
     public static function notice($message, array $context = [])
     {
-        static::log('notice', $message);
+        static::log('notice', $message, $context);
     }
 
     public static function info($message, array $context = [])
     {
-        static::log('info', $message);
+        static::log('info', $message, $context);
     }
 
     public static function debug($message, array $context = [])
     {
-        static::log('debug', $message);
+        static::log('debug', $message, $context);
     }
 
     public static function log($level, $message, array $context = [])
     {
+        if (mb_strlen($message) < 1) {
+            return;
+        }
+
         FacadesLog::log($level, $message, $context);
+
         try {
-            $channel = Tulpar::getInstance()->getLogChannel();
-            if ($channel === null) {
-                echo PHP_EOL . PHP_EOL . '!!! WARN: Log channel is not selected !!!' . PHP_EOL . PHP_EOL;
-            }
-            else {
+            $channel = static::getChannel();
+            if ($channel !== false) {
                 if (!Tulpar::getInstance()->checkPermission($channel->guild, 'administrator', true)) {
-                    echo PHP_EOL . PHP_EOL . '!!! ERR: Missing "administrator" permission in application server !!!' . PHP_EOL . PHP_EOL;
-                    Tulpar::getInstance()->getDiscord()->getLoop()->stop();
+                    static::consoleLog('Missing "administrator" permission in application server', 'ERR');
+                    Tulpar::getInstance()->stop();
                     exit;
                 }
 
                 if (in_array($level, ['emergency', 'alert', 'critical', 'error', 'warning', 'notice', 'info', 'debug'])) {
                     if (config('tulpar.server.logging.' . $level) == true) {
-                        $channel->sendMessage(
-                            '``' . strtoupper($level) . '``: ' . $message
-                        );
+                        $messages = explode("\r\n", chunk_split($message, 200));
+                        $channel->sendMessage('``' . strtoupper($level) . '``: ' . $messages[0])->done(function (Message $message) use ($messages) {
+                            $messages = collect($messages)->except(0)->toArray();
+                            foreach ($messages as $msg) {
+                                if (mb_strlen($msg) > 0) {
+                                    $message->reply($msg);
+                                }
+                            }
+                        });
                     }
                 }
             }
         } catch (Exception $exception) {
-            // ...
+            FacadesLog::critical($exception->getTraceAsString());
+            static::consoleLog('ERR', 'CANNOT SEND MESSAGE TO LOG CHANNEL SEE LOG FILE TO DETAILS');
         }
     }
 }
