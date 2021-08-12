3f26032bb1c8951c097d258a3a2bf535ac9e25f2
Isa Eken <hello@isaeken.com.tr>
2021-08-12 08:12:23 +0300

    updated help command

--- app/Tulpar/Commands/Basic/HelpCommand.php
+++ app/Tulpar/Commands/Basic/HelpCommand.php
@@ -6,7 +6,12 @@ namespace App\Tulpar\Commands\Basic;
 
 use App\Tulpar\Commands\BaseCommand;
 use App\Tulpar\Contracts\CommandInterface;
+use Discord\Builders\Components\Option;
+use Discord\Builders\Components\SelectMenu;
+use Discord\Builders\MessageBuilder;
+use Discord\Helpers\Collection;
 use Discord\Parts\Embed\Embed;
+use Discord\Parts\Interactions\Interaction;
 
 class HelpCommand extends BaseCommand implements CommandInterface
 {
@@ -20,38 +25,31 @@ class HelpCommand extends BaseCommand implements CommandInterface
 
     public static bool $allowPm = true;
 
-    public static array $usages = [
-        '',
-        'command-name',
-    ];
-
     public function run(): void
     {
         $embed = new Embed($this->discord);
         $embed->title = config('app.name') . ' ' . config('app.version');
         $embed->description = 'You can use these commands in here.';
 
-        if ($this->userCommand->hasArgument(0)) {
-            $commandName = mb_strtolower($this->userCommand->getArgument(0));
+        $builder = MessageBuilder::new();
+        $builder->setContent('Select a command to show help.');
+        $builder->setReplyTo($this->message);
 
-            /** @var BaseCommand $command */
-            foreach (config('tulpar.commands', []) as $command) {
-                if (mb_strtolower($command::getCommand()) == $commandName && (new $command($this->message, $this->discord))->checkAccess()) {
-                    $embed->addFieldValues($command::getCommand(), $command::getHelp());
-                }
-            }
-
-            $this->message->channel->sendEmbed($embed);
-            return;
-        }
-
+        $selectMenu = SelectMenu::new();
         /** @var BaseCommand $command */
         foreach (config('tulpar.commands', []) as $command) {
             if ((new $command($this->message, $this->discord))->checkAccess()) {
-                $embed->addFieldValues($command::getCommand(), $command::getHelp());
+                $selectMenu = $selectMenu->addOption(Option::new($command::getCommand(), $command::getCommand()))->setListener(function (Interaction $interaction, Collection $options) use ($command) {
+                    $embed = new Embed($this->discord);
+                    $embed->setAuthor($this->message->user->username, $this->message->user->avatar);
+                    $embed->setDescription($command::getHelp());
+
+                    $interaction->updateMessage(MessageBuilder::new()->setContent('Command details:')->addEmbed($embed));
+                }, $this->discord);
             }
         }
 
-        $this->message->channel->sendEmbed($embed);
+        $builder->addComponent($selectMenu);
+        $this->message->channel->sendMessage($builder);
     }
 }
