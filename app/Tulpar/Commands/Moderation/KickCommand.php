d31791fd6ce79edef28a147343a079312ce01e19
Isa Eken <hello@isaeken.com.tr>
2021-08-12 07:16:38 +0300

    updated kick command

--- app/Tulpar/Commands/Moderation/KickCommand.php
+++ app/Tulpar/Commands/Moderation/KickCommand.php
@@ -6,8 +6,11 @@ namespace App\Tulpar\Commands\Moderation;
 
 use App\Tulpar\Commands\BaseCommand;
 use App\Tulpar\Contracts\CommandInterface;
+use App\Tulpar\Dialog;
 use App\Tulpar\Helpers;
 use App\Tulpar\Log;
+use Discord\Builders\MessageBuilder;
+use Discord\Parts\Interactions\Interaction;
 use Discord\Parts\User\Member;
 use React\Promise\Promise;
 
@@ -27,7 +30,7 @@ class KickCommand extends BaseCommand implements CommandInterface
 
     public static array $requires = [0];
 
-    public static string $version = '1.0';
+    public static string $version = '1.3';
 
     public function run(): void
     {
@@ -40,19 +43,28 @@ class KickCommand extends BaseCommand implements CommandInterface
             return;
         }
 
-        new Promise(function () use ($member, $delay) {
-            sleep($delay);
+        $this->message->channel->sendMessage(Dialog::confirm(
+            'Are you serious to kick user: ' . Helpers::userTag($member->id),
+            listenerNo: function (Interaction $interaction, MessageBuilder $builder) {
+                $interaction->updateMessage(MessageBuilder::new()->setContent('Kicking progress is canceled.'));
+            },
+            listenerYes: function (Interaction $interaction, MessageBuilder $builder) use ($member, $delay) {
+                $interaction->updateMessage(MessageBuilder::new()->setContent('User is kicking in ' . $delay . ' seconds.'));
+                new Promise(function () use ($member, $delay) {
+                    sleep($delay);
 
-            $except = function ($exception) {
-                Log::error($exception);
-                $this->message->reply('An error occurred when kicking the user.');
-            };
+                    $except = function ($exception) {
+                        Log::error($exception);
+                        $this->message->reply('An error occurred when kicking the user.');
+                    };
 
-            $member->ban()->done(function () use ($member, $except) {
-                $this->message->channel->guild->unban($member)->done(function () use ($member) {
-                    $this->message->reply('The user ' . Helpers::userTag($member->id) . ' is kicked.');
-                }, $except);
-            }, $except);
-        });
+                    $member->ban()->done(function () use ($member, $except) {
+                        $this->message->channel->guild->unban($member)->done(function () use ($member) {
+                            $this->message->reply('The user ' . Helpers::userTag($member->id) . ' is kicked.');
+                        }, $except);
+                    }, $except);
+                });
+            }
+        )->setReplyTo($this->message));
     }
 }
