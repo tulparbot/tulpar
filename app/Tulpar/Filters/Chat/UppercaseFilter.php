35e6a342053efbc88cf564aa09b7863ae3cadab9
Isa Eken <hello@isaeken.com.tr>
2021-08-13 18:48:22 +0300

    added uppercase filter

---
+++ app/Tulpar/Filters/Chat/UppercaseFilter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tulpar\Filters\Chat;
+
+use App\Tulpar\Filters\BaseFilter;
+use Discord\Parts\Channel\Message;
+
+class UppercaseFilter extends BaseFilter
+{
+    /**
+     * @inheritDoc
+     */
+    public function check(): bool
+    {
+        $count = 0;
+        $matches = [];
+        if (preg_match_all('/[A-Z]/', $this->message->content, $matches) > 0) {
+            foreach ($matches[0] as $match) {
+                $count += mb_strlen($match);
+            }
+        }
+
+        return (($count / mb_strlen($this->message->content)) * 100) > 70;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function run(): Message
+    {
+        if ($this->check()) {
+            $this->message->reply('Do not use uppercase characters!');
+        }
+
+        return $this->message;
+    }
+}
