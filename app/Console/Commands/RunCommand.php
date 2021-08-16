d8224b642e2bcca815f0e4cc159a641e2ef7de6b
Isa Eken <hello@isaeken.com.tr>
2021-08-16 08:50:24 +0300

    update activity timer

--- app/Console/Commands/RunCommand.php
+++ app/Console/Commands/RunCommand.php
@@ -17,6 +17,7 @@ use Discord\WebSockets\Intents;
 use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Str;
 use Symfony\Component\Console\Command\Command as CommandAlias;
 
 class RunCommand extends Command
@@ -109,11 +110,17 @@ class RunCommand extends Command
                 });
             }
         });
-        static::$instance->getDiscord()->getLoop()->addPeriodicTimer(30, function () {
+        static::$instance->getDiscord()->getLoop()->addPeriodicTimer(5, function () {
             $activities = config('tulpar.activities');
+            $activity = $activities[array_rand($activities)];
+            $activity->name = Str::of($activity->name)
+                ->replace('{prefix}', Tulpar::getPrefix())
+                ->replace('{guild_count}', Tulpar::getInstance()->getDiscord()->guilds->count())
+                ->replace('{member_count}', Tulpar::getInstance()->getDiscord()->users->count())
+                ->replace('{command_count}', count(config('tulpar.commands')));
 
             /** @var Activity $_ */
-            $_ = static::$instance->getDiscord()->factory(\Discord\Parts\User\Activity::class, $activities[array_rand($activities)]);
+            $_ = static::$instance->getDiscord()->factory(\Discord\Parts\User\Activity::class, $activity);
             static::$instance->getDiscord()->updatePresence($_);
         });
     }
