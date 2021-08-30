738a462fc2e0ab4404acb521c25bb51cac453f51
Isa Eken <hello@isaeken.com.tr>
2021-08-30 18:08:30 +0300

    update temp-ban command

--- app/Tulpar/Commands/Moderation/TempBanCommand.php
+++ app/Tulpar/Commands/Moderation/TempBanCommand.php
@@ -7,6 +7,7 @@ namespace App\Tulpar\Commands\Moderation;
 use App\Enums\CommandCategory;
 use App\Models\TempBan;
 use App\Tulpar\Commands\BaseCommand;
+use App\Tulpar\CommandTraits\HasMemberArgument;
 use App\Tulpar\Contracts\CommandInterface;
 use App\Tulpar\Logger;
 use Discord\Parts\User\Member;
@@ -15,6 +16,8 @@ use Illuminate\Support\Carbon;
 
 class TempBanCommand extends BaseCommand implements CommandInterface
 {
+    use HasMemberArgument;
+
     public static string $command = 'temp-ban';
 
     public static string $description = 'Temporary ban the user.';
@@ -28,16 +31,14 @@ class TempBanCommand extends BaseCommand implements CommandInterface
 
     public static array $requires = [0];
 
-    public static string $version = '1.0';
+    public static string $version = '1.1';
 
     public static string $category = CommandCategory::Moderation;
 
     public function run(): void
     {
-        /** @var Member $member */
-        $member = $this->message->channel->guild->members->get('id', $this->userCommand->getArgument(0));
+        $member = $this->getMemberArgument(0, true);
         if (!$member instanceof Member) {
-            $this->message->reply($this->translate('You can only ban members!'));
             return;
         }
 
